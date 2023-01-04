45bbec00391a1459c36852086ea780a1287d1b6a
Fernando Santos Moreno <fernando.santosm@gmail.com>
2023-01-04 10:51:03 -0300

    CProject v1.2 - Vetores
    Finalização da sessão de vetor.

--- vetor.c
+++ vetor.c
@@ -6,38 +6,52 @@
 /*
  * 
  */
-void geraVetor(float[]);
+void vetor();
+void geraVetor(float[],int);
 void exibeVetor(float[],int);
-float maior(float[]);
-float menor(float[]);
-float media(float[]);
-float mediana(float[]);
-void crescente(float[]);
-void decrescente(float[]);
+float soma(float[],int);
+float maior(float[],int);
+float menor(float[],int);
+float media(float[],int);
+float mediana(float[],int);
+void crescente(float[],int);
+void decrescente(float[],int);
 
 void vetor(){
     system("cls");
-    float vet[TAM];
-    int i;
+    int i,tam;
 
     
     printf("Bem vindo ao submenu de vetores!\n");
     printf("Aqui voce pode visualizar diversas manipulacoes com vetores!\n\n");
     
-    geraVetor(vet);
+    tam=TAM;
+    float vet[tam];
+    geraVetor(vet,tam);
     
-    printf("Vetor gerado pelo sistema: \n");
-   
-    exibeVetor(vet,TAM);
+    
+    printf("Vetor de tamanho %d gerado pelo sistema: \n",tam);
+    exibeVetor(vet,tam);
+    printf("O somatorio dos numeros e: %0.1f\n",soma(vet,tam));
+    printf("O maior valor do vetor e: %0.1f\n",maior(vet,tam));
+    printf("O menor valor do vetor e: %0.1f\n",menor(vet,tam));
+    printf("O valor medio do vetor e: %0.1f\n",media(vet,tam));
+    printf("O valor mediano do vetor e: %0.1f\n",mediana(vet,tam));
+    printf("O vetor na ordem crescente e:\n");
+    crescente(vet,tam);
+    exibeVetor(vet,tam);
+    printf("O vetor na ordem decrescente e:\n");
+    decrescente(vet,tam);
+    exibeVetor(vet,tam);
     
     printf("\n\n");
     
 }
 
-void geraVetor(float vet[]){
+void geraVetor(float vet[],int tam){
     int i;
     
-    for(i=0;i<TAM;i++){
+    for(i=0;i<tam;i++){
         vet[i] = rand()%10;
     } 
 }
@@ -48,34 +62,80 @@ void exibeVetor(float vet[],int tam){
     for(i=0;i<tam;i++){ 
         printf("[%.1f] ",vet[i]);
     }
+    
+    printf("\n");
 }
 
-float maior(float vet[]){
+float soma(float vet[],int tam){
+    int i;
+    float s=0;
+    
+    for(i=0;i<tam;i++){ 
+        s+=vet[i];
+    }
+    return s;
+}
+
+float maior(float vet[],int tam){
     int i;
     float m = vet[0];
-    for (i=1;i<TAM;i++){
+    for (i=1;i<tam;i++){
         if(m<vet[i]) m = vet[i];
     }
     return m;
 }
-float menor(float vet[]){
+
+float menor(float vet[],int tam){
     int i;
     float m = vet[0];
-    for (i=i;i<TAM;i++){
+    for (i=i;i<tam;i++){
         if(m > vet[i]) m = vet[i];
     }
     return m;
 }
-float media(float vet[]){
+
+float media(float vet[],int tam){
     int i;
     float m;
-    crescente(vet);
-    
-    TAM/2? m = vet[TAM/2] : m = (vet[TAM/2]+vet[(TAM/2)-1])/2;
+
+    m=soma(vet,tam)/tam;
     
     return m;
-    
 }
-float mediana(float[]);
-void crescente(float[]);
-void decrescente(float[]);
+
+float mediana(float vet[],int tam){
+    crescente(vet,tam);
+    float m=0;
+    
+    tam/2? m = vet[tam/2] : m = ((vet[(tam/2)]+vet[(tam/2)-1])/2);
+    
+    return m;
+}
+
+void crescente(float vet[], int tam){
+    int i,j;
+    float aux;
+    for(i=0;i<tam;i++){
+        for(j=i+1;j<tam;j++){
+            if(vet[j]<vet[i]){
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
+        }
+    }
+}
+
+void decrescente(float vet[], int tam){
+    int i,j;
+    float aux;
+    for(i=0;i<tam;i++){
+        for(j=i+1;j<tam;j++){
+            if(vet[j]>vet[i]){
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
+        }
+    }
+}
